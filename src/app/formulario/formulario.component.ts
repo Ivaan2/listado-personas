884fa9b6315980fdaed13b1684518c14fe6c29b2
Ivaan2 <ivan.m.m.2002.ivi@gmail.com>
2022-07-20 10:50:29 +0200

    App Routing Module

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -9,15 +9,16 @@ import { PersonasService } from '../persona.service';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent  {
-  nombreInput:string;
-  apellidoInput:string;  
+  nombreInput:string = '';
+  apellidoInput:string = '';  
 
   constructor(private loggingService:LoggingService,
     private personaService:PersonasService){}
 
   agregarPersona(){
-    let persona1 = new Persona(this.nombreInput, this.apellidoInput);
-    this.personaService.agregarPersona(persona1);
+    if(this.nombreInput!='' && this.apellidoInput!=''){
+      let persona1 = new Persona(this.nombreInput, this.apellidoInput);
+      this.personaService.agregarPersona(persona1);
+    }
   }
-
 }
